81131839142d98cbe30bf68a47b7112c5bbee4df
kOOnzTe <alperwitsteel@gmail.com>
2025-05-11 19:41:43 +0300

    Remove redundant comment about "isRegistered" field in User class

--- src/users/User.java
+++ src/users/User.java
@@ -3,6 +3,7 @@ package users;
 public class User {
     protected String name;
     protected boolean isChild;
+    // no need to add "isRegistered" as a field, since we are registering users via ".register()" method with the help of observers
 
     public User(String name, boolean isChild) {
         this.name = name;
